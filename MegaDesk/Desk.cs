f885b31b122c3d6e80680679d7e96e8dc0f1bcac
calva-alexander <38995873+alexandercalva@users.noreply.github.com>
2020-01-25 23:37:01 -0500

    Enum
    Desk with const

--- MegaDesk/Desk.cs
+++ MegaDesk/Desk.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 
 namespace MegaDesk
 {
+    enum Material { Laminate, Oak, Rosewood, Venner, Pine }
     class Desk
     {
+        private const int WIDTHMAX = 96, WITHMIN = 24, DEPTHMAX = 48, DEPTHMIN = 12;
     }
 }
