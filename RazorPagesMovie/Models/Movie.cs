da7a0dc31eedba34eafa83f1265a5a287e9fa1f5
calva-alexander <38995873+alexandercalva@users.noreply.github.com>
2020-02-15 10:22:39 -0500

    Searching

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -7,6 +7,7 @@ using System.IO;
 using NuGet;
 using Cake.Incubator.Project;
 using PackageReference = NuGet.PackageReference;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RazorPagesMovie.Models
 {
@@ -15,9 +16,11 @@ namespace RazorPagesMovie.Models
         public int ID { get; set; }
         public string Title { get; set; }
 
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
