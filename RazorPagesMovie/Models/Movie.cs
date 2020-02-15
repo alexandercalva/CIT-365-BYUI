88167a539c173901c220c4d68e284a66d8d21a00
calva-alexander <38995873+alexandercalva@users.noreply.github.com>
2020-02-15 00:45:22 -0500

    DataBase
    Work with the database

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using System.IO;
+using NuGet;
+using Cake.Incubator.Project;
+using PackageReference = NuGet.PackageReference;
 
 namespace RazorPagesMovie.Models
 {
     public class Movie
     {
-
         public int ID { get; set; }
         public string Title { get; set; }
 
