4c642a349c1f71bd39134d367cf4de2772c33446
calva-alexander <38995873+alexandercalva@users.noreply.github.com>
2020-02-15 00:05:48 -0500

    Test

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,13 @@ namespace RazorPagesMovie.Models
 {
     public class Movie
     {
-    
-    //Probando github
-    
+
+        public int ID { get; set; }
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
     }
 }
