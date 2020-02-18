f7c0ec40ab4de5d1a98b87a18348b336ddcad24b
calva-alexander <38995873+alexandercalva@users.noreply.github.com>
2020-02-18 16:50:26 -0500

    Searching completed
    By books and words in Notes

--- MyScriptureJournal/Pages/Scriptures/Index.cshtml.cs
+++ MyScriptureJournal/Pages/Scriptures/Index.cshtml.cs
@@ -24,32 +24,44 @@ namespace MyScriptureJournal
         [BindProperty(SupportsGet = true)]
         public string SearchString { get; set; }
         // Requires using Microsoft.AspNetCore.Mvc.Rendering;
-        public SelectList Books { get; set; }
+        public SelectList Book { get; set; }
         [BindProperty(SupportsGet = true)]
-        public string Note { get; set; }
+        public string BookList { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string NoteSearch { get; set; }
         public async Task OnGetAsync()
         {
             // Use LINQ to get list of genres.
             IQueryable<string> BookQuery = from m in _context.Scripture
                                             orderby m.Book
                                             select m.Book;
-
+            IQueryable<string> NoteQuery = from m in _context.Scripture
+                                           orderby m.Note
+                                           select m.Note;
 
             var scriptures = from m in _context.Scripture
                          select m;
+            var note = from n in _context.Scripture select n;
+
             if (!string.IsNullOrEmpty(SearchString))
             {
                 scriptures = scriptures.Where(s => s.Book.Contains(SearchString));
             }
-            Scripture = await _context.Scripture.ToListAsync();
-
-            if (!string.IsNullOrEmpty(Note))
+            if (!string.IsNullOrEmpty(NoteSearch))
             {
-                scriptures = scriptures.Where(x => x.Note == Note);
+                scriptures = scriptures.Where(s => s.Note.Contains(NoteSearch));
             }
-           Books = new SelectList(await BookQuery.Distinct().ToListAsync());
-            Scripture = await scriptures.ToListAsync();
+            //Scripture = await _context.Scripture.ToListAsync();
 
+            if (!string.IsNullOrEmpty(BookList))
+            {
+                scriptures = scriptures.Where(x => x.Book == BookList);
+            }
+           Book = new SelectList(await BookQuery.Distinct().ToListAsync());
+
+            //Note = new Search(await NoteQuery.Distinct().ToListAsync());
+            
+            Scripture = await scriptures.ToListAsync();
 
         }
     }
