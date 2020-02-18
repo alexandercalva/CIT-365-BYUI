dfd0643ea8a6315882690dace653031b11d755a6
calva-alexander <38995873+alexandercalva@users.noreply.github.com>
2020-02-17 19:56:05 -0500

    Scaffolding

---
+++ MyScriptureJournal/Models/Scripture.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyScriptureJournal.Models
+{
+    public class Scripture
+    {
+        public int ID { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+        public string Book { get; set; }
+             
+        public string Chapter { get; set; }
+        public decimal Verse { get; set; }
+    }
+}
