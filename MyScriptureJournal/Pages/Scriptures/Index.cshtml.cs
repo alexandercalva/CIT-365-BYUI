00e3118d98b33fd55ab3f42c3bce7401d705b17a
calva-alexander <38995873+alexandercalva@users.noreply.github.com>
2020-02-19 02:33:55 -0500

    Sort
    In process

--- MyScriptureJournal/Pages/Scriptures/Index.cshtml.cs
+++ MyScriptureJournal/Pages/Scriptures/Index.cshtml.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.Web.CodeGeneration.Contracts.Messaging;
 using MyScriptureJournal.Data;
 using MyScriptureJournal.Models;
 
@@ -19,8 +20,12 @@ namespace MyScriptureJournal
         {
             _context = context;
         }
+        [BindProperty(SupportsGet = true)]
+        public string BookSort { get; set; }
+        public string DateSort { get; set; }
 
-        public IList<Scripture> Scripture { get;set; }
+       
+        public IList<Scripture> ScriptureList { get;set; }
         [BindProperty(SupportsGet = true)]
         public string SearchString { get; set; }
         // Requires using Microsoft.AspNetCore.Mvc.Rendering;
@@ -29,20 +34,50 @@ namespace MyScriptureJournal
         public string BookList { get; set; }
         [BindProperty(SupportsGet = true)]
         public string NoteSearch { get; set; }
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string sortOrder)
         {
-            // Use LINQ to get list of genres.
+            BookSort = String.IsNullOrEmpty(sortOrder) ? "book_desc" : "";
+            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+            // Use LINQ 
+            IQueryable<Scripture> BookST = from m in _context.Scripture
+                                          
+                                        select m;
             IQueryable<string> BookQuery = from m in _context.Scripture
-                                            orderby m.Book
                                             select m.Book;
             IQueryable<string> NoteQuery = from m in _context.Scripture
                                            orderby m.Note
                                            select m.Note;
+            IQueryable<DateTime> DateSortQuery = from m in _context.Scripture
+                                               orderby m.Date
+                                               select m.Date;
+
+            switch (sortOrder)
+            {
+                case "book_desc":
+
+                    //BookST = BookST.OrderByDescending(s => s.Book.Contains(BookList));
+                    BookST = BookST.Where(s => s.Book.Contains(BookSort));
+                    break;
+                case "Date":
+                    BookST = BookST.OrderBy(s => s.Date);
+                    break;
+                case "date_desc":
+                    BookST = BookST.OrderByDescending(s => s.Date);
+                    break;
+                default:
+                    BookST = BookST.OrderBy(s => s.Book.Contains(BookSort));
+                    break;
+            }
+
+
+           // ScriptureList = await BookST.ToListAsync();
 
             var scriptures = from m in _context.Scripture
-                         select m;
+                        select m;
             var note = from n in _context.Scripture select n;
+           
 
+           
             if (!string.IsNullOrEmpty(SearchString))
             {
                 scriptures = scriptures.Where(s => s.Book.Contains(SearchString));
@@ -57,11 +92,11 @@ namespace MyScriptureJournal
             {
                 scriptures = scriptures.Where(x => x.Book == BookList);
             }
-           Book = new SelectList(await BookQuery.Distinct().ToListAsync());
-
+             Book = new SelectList(await BookQuery.Distinct().ToListAsync());
+            //var DateSort = await DateSortQuery.Distinct().ToListAsync();
             //Note = new Search(await NoteQuery.Distinct().ToListAsync());
-            
-            Scripture = await scriptures.ToListAsync();
+
+            ScriptureList = await scriptures.ToListAsync();
 
         }
     }
